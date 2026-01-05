f5eba16f955f717171c323e4dcd40eb070df8dae
Aren Bruce <devtodaydevtomorrow@gmail.com>
2026-01-05 12:33:07 -0500

    project scaffolding

---
+++ src/app/core/services/note.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Note } from '../models/note.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteService {
+
+  constructor() { 
+
+
+  }
+}
